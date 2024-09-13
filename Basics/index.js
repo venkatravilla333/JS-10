dfac3579e05363f1e21eddafb8e4f226f55c03e3
venkat <venkatravilla999@gmail.com>
2024-09-13 14:40:48 +0530

    upto operators

---
+++ Basics/index.js
@@ -0,0 +1,75 @@
+//primitives
+
+// var a = 'sachin'
+// console.log(a, typeof a)
+
+// var b = 40
+// console.log(b, typeof b)
+
+// var c = true
+// console.log(c, typeof c)
+
+// var d = undefined
+// console.log(d, typeof d)
+
+// var e = null
+// console.log(e, typeof e)
+
+//reference data types
+
+// var person = {
+//   name: 'sachin',
+//   age: 40,
+//   city: 'mumbai',
+//   play: () => {
+//     console.log('play')
+//   }
+// }
+
+// console.log(person, typeof person)
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person.city)
+// person.play()
+// console.log(person['name'])
+// console.log(person['age'])
+// console.log(person['city'])
+// person['play()']
+
+// console.log(typeof(typeof person.name))
+// console.log(typeof(typeof person.age))
+// console.log(typeof(typeof person.city))
+// console.log(typeof(typeof person.play))
+
+
+//Arrays
+
+// var names = ['sachin', 'kohli', 'dhoni', 100, true, null]
+
+// console.log(names, typeof names)
+// console.log(Array.isArray(names))
+// console.log(names[0])
+// console.log(names[1])
+// console.log(names[2])
+// console.log(names[3])
+// console.log(names[4])
+// console.log(names[5])
+
+
+
+//functions
+
+// var x = 10
+// var test = () => {
+//   var x = 100
+// }
+// test()
+
+// console.log(test, typeof test)
+
+
+
+
+
+
+ 
---
+++ Operators/index.js
@@ -0,0 +1,119 @@
+
+
+//arithmatic operators
+
+// var x = 20
+// var y = 10
+
+// console.log(x + y)  //add
+// console.log(x - y)  //substraction
+// console.log(x * y)  //multiplication
+// console.log(x / y)  //division
+// console.log(x % y)  //remainder
+// console.log(x ** y)  //exponential
+
+// incremental
+
+// console.log(x)
+// console.log(++x) //pre
+// console.log(++x)  //pre
+// console.log(x++)  //post
+// console.log(x++)  //post
+// console.log(x)
+
+//decrement
+
+// console.log(--x) //pre
+// console.log(--x) //pre
+// console.log(x--) //post
+// console.log(x--)  //post
+// console.log(x)
+
+
+//assignment operator (=)
+
+// var x = 10
+
+// console.log(x++)
+// console.log(x++)
+// console.log(x++)
+// console.log(x++)
+// console.log(x++)
+// console.log(x)
+
+//assignment & arithmatic operators
+
+// console.log(x += 5)
+// console.log(x -= 5)
+// console.log(x *= 5)
+// console.log(x /= 5)
+// console.log(x %= 5)
+// console.log(x **= 5)
+
+//comparision operators
+
+var x = 10
+var y = 11
+
+//relational
+
+// console.log(x > y)
+// console.log(x >= y)
+// console.log(x < y)
+// console.log(x <= y)
+
+//equality
+
+
+//primitives
+
+// var x = 10
+// var y = 10
+// var y = '10'
+// var y = 11
+// var x = undefined
+// var y = null
+
+//reference
+
+// var x = {}
+// var x = {value: 10}
+// var y = {}
+// var y = {value: 10}
+// var y = x
+
+// var x = [1, 2, 3]
+// var y = [10, 20, 30]
+
+// var x = 10
+// var y = []
+
+// var x = [1,2,3]
+// var y = ()=>{}
+
+// console.log(x==y) //loose (compares values)
+// console.log(x===y) //strict (compares values and its data types)
+
+//ternary operator
+
+// var age = 18
+
+// var result = age >= 18 ? 'having vote right' : 'not having vote right'
+// console.log(result)
+
+//logical operators
+
+var income = false
+var cibil = true
+var local = true
+
+// var icicicc = income && cibil && local //logical AND
+// var rblcc =   income || cibil && local //logical OR
+// console.log(icicicc)
+// console.log(rblcc)
+
+console.log(income)
+console.log(!income) //not operator
+
+
+
