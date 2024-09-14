896c18f5d4a1f6cd7eebd9dc463b23a9593154c0
venkat <venkatravilla999@gmail.com>
2024-09-14 10:50:25 +0530

    content upto operators

---
+++ Control-Statements/index.js
@@ -0,0 +1,135 @@
+
+
+//marks
+
+// var marks = 75
+
+// if else
+
+// if (marks < 35) {
+  //   console.log('fail')
+  // } else if (marks >= 35 && marks < 60) {
+    //   console.log('second class')
+// } else if (marks >= 60 && marks < 75) {
+//   console.log('first class')
+// } else {
+//   console.log('distinction')
+// }
+
+//switch case
+
+// var marks = 20
+
+// switch (marks) {
+//   case marks < 35:
+//     console.log('fail')
+//     break;
+//   case marks >= 35 && marks < 60:
+//     console.log('second class')
+//     break;
+//   case marks >= 60 && marks < 75:
+//     console.log('first class')
+//     break;
+//   default:
+//     console.log('distinction')
+  
+// }
+
+// var num = 0
+
+// if (num > 0) {
+//   console.log('positive')
+// } else if (num < 0) {
+//   console.log('negative')
+// } else {
+//   console.log('Zero')
+// }
+
+// var num = 10
+
+// switch (num) {
+//   case num > 0:
+//     console.log('positive')
+//     break;
+//   case num < 0:
+//     console.log('negative')
+//     break;
+//   default:
+//     console.log('Zero')
+// }
+
+//
+
+// var customer = 'Gold'
+// switch (customer) {
+//   case 'Gold':
+//     console.log('gold customer')
+//     break
+//   case 'Silver':
+//     console.log('silver customer')
+//     break;
+//   case 'Iron':
+//     console.log('iron customer')
+//     break;
+//   default:
+//     console.log('unknown customer')
+// }
+
+// var customer = 'Gold'
+
+// if (customer === 'Gold') {
+//   console.log('gold customer')
+// } else if (customer === 'Silver') {
+//   console.log('silver customer')
+// } else if (customer === 'Iron') {
+//   console.log('iron customer')
+// } else {
+//   console.log('unknown customer')
+// }
+
+// var weekDays = 8
+
+// if (weekDays === 1) {
+//   console.log('mon')
+// } else if (weekDays === 2) {
+//   console.log('thues')
+// } else if (weekDays === 3) {
+//   console.log('wed')
+// } else if (weekDays === 4) {
+//   console.log('thus')
+// } else if (weekDays === 5) {
+//   console.log('fri')
+// } else if (weekDays === 6) {
+//   console.log('sat')
+// } else if (weekDays === 7) {
+//   console.log('sun')
+// } else {
+//   console.log('invalid weekday')
+// }
+
+// var weekday = 3
+// switch (weekday) {
+//   case 1:
+//     console.log('mon')
+//     break;
+//   case 2:
+//     console.log('thues')
+//     break;
+//   case 3:
+//     console.log('wed')
+//     break;
+//   case 4:
+//     console.log('thurs')
+//     break;
+//   case 5:
+//     console.log('fri')
+//     break;
+//   case 6:
+//     console.log('sat')
+//     break;
+//   case 7:
+//     console.log('sun')
+//     break;
+//   default:
+//     console.log('invalid weekday')
+// }
